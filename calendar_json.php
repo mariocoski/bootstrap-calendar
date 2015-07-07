b1acc89281d6232668cbed0ef66ecb93ff3fe5e8
Mariocoski <Mariocoski@Mariocoski-PC>
2015-07-07 21:59:49 +0100

    calendar changes

---
+++ calendar_json.php
@@ -0,0 +1,101 @@
+<div class="horizontal-calendar-big-content">  
+  <table class="table">
+    <tr class="horizontal-calendar-big-day-names">  
+      <td>Monday</td>    
+      <td>Tuesday</td> 
+      <td>Wednesday</td> 
+      <td>Thursday</td> 
+      <td>Friday</td> 
+      <td>Saturday</td> 
+      <td>Sunday</td> 
+    </tr>  
+    <tr class="horizontal-calendar-day-numbers">  
+      <td>06 November</td>    
+      <td>07 November</td> 
+      <td>08 November</td> 
+      <td>09 November</td> 
+      <td>10 November</td> 
+      <td>11 November</td> 
+      <td>12 November</td> 
+    </tr>
+    <tr class="horizontal-calendary-day-hours">
+      <td>
+        <ul class="list-unstyled ">
+          <li class="horizontal-calendar-big-list-item"><a rel="nofollow" class="horizontal-calendar-big-link" href="">8:00</a></li>
+          <li class="horizontal-calendar-big-list-item"><a rel="nofollow" class="horizontal-calendar-big-link" href="">9:00</a></li>
+          <li class="horizontal-calendar-big-list-item"><a rel="nofollow" class="horizontal-calendar-big-link" href="">10:00</a></li>
+          <li class="horizontal-calendar-big-list-item"><a rel="nofollow" class="horizontal-calendar-big-link" href="">11:00</a></li>
+          <li class="horizontal-calendar-big-list-item"><a rel="nofollow" class="horizontal-calendar-big-link" href="">12:00</a></li>
+          <li class="horizontal-calendar-big-list-item"><a rel="nofollow" class="horizontal-calendar-big-link" href="">13:00</a></li>
+          <li class="horizontal-calendar-big-list-item"><a rel="nofollow" class="horizontal-calendar-big-link" href="">14:00</a></li>
+          <li><a rel="nofollow" class="horizontal-calendar-big-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="">more</a>
+              <ul class="dropdown-menu ">
+                <li rel="nofollow" class="horizontal-calendar-big-booked"><span class="booked-visit">12.30</span></li>
+                <li><a rel="nofollow" class="" href="">18:00</a></li>
+                <li><a rel="nofollow" class="" href="">8:00</a></li>
+                <li><a rel="nofollow" class="" href="">8:00</a></li>
+                <li ><a rel="nofollow" class="" href="">18:00</a></li>
+                <li rel="nofollow" class="horizontal-calendar-big-booked"><span class="booked-visit">12.30</span></li>
+                <li ><a rel="nofollow" class="" href="">8:00</a></li>
+                <li ><a rel="nofollow" class="" href="">8:00</a></li>
+                <li ><a rel="nofollow" class="" href="">18:00</a></li>
+                <li rel="nofollow" class="horizontal-calendar-big-booked"><span class="booked-visit">12.30</span></li>
+              </ul>
+          </li>
+        </ul>
+      </td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>
+          <ul class="list-unstyled ">
+          <li class="horizontal-calendar-big-list-item"><a rel="nofollow" class="horizontal-calendar-big-link" href="">8:00</a></li>
+          <li class="horizontal-calendar-big-list-item"><a rel="nofollow" class="horizontal-calendar-big-link" href="">9:00</a></li>
+          <li class="horizontal-calendar-big-list-item"><a rel="nofollow" class="horizontal-calendar-big-link" href="">10:00</a></li>
+          <li class="horizontal-calendar-big-list-item"><a rel="nofollow" class="horizontal-calendar-big-link" href="">11:00</a></li>
+          <li class="horizontal-calendar-big-list-item"><a rel="nofollow" class="horizontal-calendar-big-link" href="">12:00</a></li>
+          <li class="horizontal-calendar-big-list-item"><a rel="nofollow" class="horizontal-calendar-big-link" href="">13:00</a></li>
+          <li class="horizontal-calendar-big-list-item"><a rel="nofollow" class="horizontal-calendar-big-link" href="">14:00</a></li>
+          <li><a rel="nofollow" class="horizontal-calendar-big-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="">more</a>
+              <ul class="dropdown-menu ">
+                <li rel="nofollow" class="horizontal-calendar-big-booked"><span class="booked-visit">12.30</span></li>
+                <li><a rel="nofollow" class="" href="">18:00</a></li>
+                <li><a rel="nofollow" class="" href="">8:00</a></li>
+                <li><a rel="nofollow" class="" href="">8:00</a></li>
+                <li ><a rel="nofollow" class="" href="">18:00</a></li>
+                <li rel="nofollow" class="horizontal-calendar-big-booked"><span class="booked-visit">12.30</span></li>
+                <li ><a rel="nofollow" class="" href="">8:00</a></li>
+                <li ><a rel="nofollow" class="" href="">8:00</a></li>
+                <li ><a rel="nofollow" class="" href="">18:00</a></li>
+                <li rel="nofollow" class="horizontal-calendar-big-booked"><span class="booked-visit">12.30</span></li>
+              </ul>
+          </li>
+        </ul>      
+      </td>
+       
+    </tr>
+  </table>
+</div><!--bootstrap-calendar-->
+
+<?php
+
+/* 
+ * SELECT mariocoski_event.*
+FROM mariocoski_event
+INNER JOIN mariocoski_user_calendar
+ON mariocoski_event.calendar_id = mariocoski_user_calendar.calendar_id
+INNER JOIN mariocoski_user
+ON mariocoski_user.id = mariocoski_user_calendar.user_id
+INNER JOIN mariocoski_calendar
+ON mariocoski_calendar.calendar_id = mariocoski_user_calendar.calendar_id
+WHERE mariocoski_user.id ='1' AND mariocoski_calendar.calendar_id='334455'        
+ * 
+ * 
+ * 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
